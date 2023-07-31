708abdd0b365620994ce56a30f351a6360d86b0b
harshoverseer <underseer@gmail.com>
2023-07-31 19:57:58 +1000

    fix service

--- app/src/main/java/com/embry/dodgeyaccessibilityservice/DodgeyAccessibilityService.kt
+++ app/src/main/java/com/embry/dodgeyaccessibilityservice/DodgeyAccessibilityService.kt
@@ -2,14 +2,31 @@ package com.embry.dodgeyaccessibilityservice
 
 import android.accessibilityservice.AccessibilityService
 import android.accessibilityservice.AccessibilityServiceInfo
+import android.annotation.SuppressLint
+import android.media.AudioManager
+import android.media.AudioManager.ADJUST_RAISE
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 
-class DodgeyAccessibilityService: AccessibilityService() {
+class DodgeyAccessibilityService : AccessibilityService() {
+
     private var info = AccessibilityServiceInfo()
+
+    @SuppressLint("ServiceCast")
+    private var audioManager: AudioManager? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+    }
+
 
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         Log.v("TAGGARTY", "$p0?.isAccessibilityDataSensitive ?: false")
+
+        if (p0?.source?.text == "Increase volume") {
+            audioManager?.adjustStreamVolume(AudioManager.STREAM_ACCESSIBILITY, ADJUST_RAISE, 0)
+        }
     }
 
     override fun onInterrupt() {
@@ -20,12 +37,8 @@ class DodgeyAccessibilityService: AccessibilityService() {
         info.apply {
             // Set the type of events that this service wants to listen to. Others
             // won't be passed to this service.
-            eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED or AccessibilityEvent.TYPE_VIEW_FOCUSED
-
-            // If you only want this service to work with specific applications, set their
-            // package names here. Otherwise, when the service is activated, it will listen
-            // to events from all applications.
-            packageNames = arrayOf("com.example.android.myFirstApp", "com.example.android.mySecondApp")
+            eventTypes =
+                AccessibilityEvent.TYPE_VIEW_CLICKED or AccessibilityEvent.TYPE_VIEW_FOCUSED
 
             // Set the type of feedback your service will provide.
             feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN
